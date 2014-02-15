989b021dc292f7294a452d0a5f309329975e4da1
chrisamanse <christopheramanse@gmail.com>
2014-02-15 16:11:37 +0800

    Changed from Students.cs to Student.cs

--- KickstarterADMU1/Models/Student.cs
+++ KickstarterADMU1/Models/Student.cs
@@ -5,13 +5,13 @@ using System.Web;
 
 namespace KickstarterADMU1.Models
 {
-    public class Students
+    public class Student
     {
         public int id { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string password { get; set; }
-        public string gender { get; set; }
+        public bool gender { get; set; }
         public int age { get; set; }
         public string course { get; set; }
         public double accountBalance { get; set; }
