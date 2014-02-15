04a10a99b509886cb3ca531409829b2d475f6bdc
chrisamanse <christopheramanse@gmail.com>
2014-02-15 12:15:49 +0800

    Uploaded Project

---
+++ KickstarterADMU1/Models/Students.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KickstarterADMU1.Models
+{
+    public class Students
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string course { get; set; }
+        public double accountBalance { get; set; }
+        public DateTime dateCreated { get; set; }
+    }
+}
